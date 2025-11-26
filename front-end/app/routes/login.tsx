a88ba89be312cc6e61cbf84b7de3f3a7b62c161b
franckarmand <koua.kouassi20@inphb.ci>
2025-11-26 06:58:57 +0000

    mise à jour

--- front-end/app/routes/login.tsx
+++ front-end/app/routes/login.tsx
@@ -118,16 +118,6 @@ export default function Login() {
                 </div>
               </div>
 
-              {/* Remember & Forgot */}
-              <div className="flex items-center justify-between">
-                <label className="flex items-center">
-                  <input type="checkbox" className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500" />
-                  <span className="ml-2 text-sm text-gray-600">Se souvenir de moi</span>
-                </label>
-                <button className="text-sm text-blue-600 hover:text-blue-700 font-medium">
-                  Mot de passe oublié ?
-                </button>
-              </div>
 
               {/* Error Message */}
               {error && (
